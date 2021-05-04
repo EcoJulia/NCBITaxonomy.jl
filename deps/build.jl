5e40bc3b9fbc4f60bd47cab01bc330c64216f3d9
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-05-04 08:58:27 -0400

    no change in unique names

--- deps/build.jl
+++ deps/build.jl
@@ -115,7 +115,7 @@ ncbi_names = DataFrames.DataFrame(tax_id=Int[], name=String[], unique_name=Union
 names_df = _build_arrow_file(ncbi_names, ncbi_names_file_in)
 names_df.class = Int.(names_df.class)
 names_df.name = lowercase.(names_df.name)
-names_df.unique_name = lwnotmis.(names_df.unique_name)
+#names_df.unique_name = lwnotmis.(names_df.unique_name)
 Arrow.write(ncbi_names_file_out, names_df)
 names_df = nothing
 GC.gc()
